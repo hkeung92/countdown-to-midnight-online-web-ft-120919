b8d739c3ddd34ebae9e710307781507b57dc1bf4
Ho Yin Keung <hykeung92@gmail.com>
2019-12-11 00:05:43 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,10 +1,10 @@
 #write your code here
 
 def countdown
-  d = 10
-  until d == 0
-     puts "#{d} SECOND(S)!"
-     d -= 1
+  countdown_time = 10
+  until countdown_time == 0
+     puts "#{countdown_time} SECOND(S)!"
+     countdown_time -= 1
   end
   puts "HAPPY NEW YEAR!"
 end
